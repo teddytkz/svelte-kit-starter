51c524c999b67c19ed85b104f83ab52f1054a27e
teddytkz <tkzteddy@gmail.com>
2026-06-17 10:47:38 +0700

    feat: add Tailwind CSS integration and custom button component
    - Updated package.json to include Tailwind CSS, font sources, and additional dependencies.
    - Created app.css for global styles and Tailwind CSS imports.
    - Implemented a custom Button component with variants and sizes using Tailwind Variants.
    - Modified layout and page components to utilize the new Button component and include global styles.
    - Added a components.json configuration for Shadcn Svelte.
    - Introduced utility functions for class name management.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,17 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+  import {Button} from "$lib/components/ui/button";
+</script>
+
+<h1 class="text-3xl">
+  Hello world!
+</h1>
+
+<Button>Click Me</Button>
+<Button variant="outline">Click Me</Button>
+<Button variant="secondary">Click Me</Button>
+<style lang="postcss">
+  @reference "tailwindcss";
+  :global(html) {
+    background-color: theme(--color-gray-100);
+  }
+</style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import adapter from '@sveltejs/adapter-node';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
 	plugins: [
+		tailwindcss(),
 		sveltekit({
 			compilerOptions: {
 				// Force runes mode for the project, except for libraries. Can be removed in svelte 6.
